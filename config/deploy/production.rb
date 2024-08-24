03c00f132c62e6e157131c95a2e2bb5bc61935e6
ruu2023 <125800583+ruu2023@users.noreply.github.com>
2024-08-24 13:46:04 +0900

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'kkappservice.com', user: 'vpsuser', roles: %w{app db web}
+server '160.251.167.135', user: 'vpsuser', roles: %w{app db web}
 
 
 # role-based syntax
